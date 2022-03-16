e9d960d4aaa215e5e3b9b7d91c8386db883dc5fa
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-16 17:25:43 -0300

    Encerramos o bloco de codigo 3.3.5 da apostila 02, sobre JS

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
-const axios = require ('axios')
-// const pessoa = {
-//     nome: "Joao",
-//     idade: 17,
-//     altura: 180
-// }
-// const {idade, nome} = pessoa
+// require('dotenv').config()
+// const axios = require ('axios')
+// // const pessoa = {
+// //     nome: "Joao",
+// //     idade: 17,
+// //     altura: 180
+// // }
+// // const {idade, nome} = pessoa
 
-const { PROTOCOL, BASE_URL, APPID, UNITS, CNT, Q, LANGUAGE} = process.env
+// const { PROTOCOL, BASE_URL, APPID, UNITS, CNT, Q, LANGUAGE} = process.env
 
-const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&lang=${LANGUAGE}&units=${UNITS}`
+// const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&lang=${LANGUAGE}&units=${UNITS}`
 
 //https://exemplo.com.br/produtos
 // app.get ('/produtos', (req, res) => {
@@ -24,17 +24,31 @@ const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&lang=${L
 // console.log(2 + 3)
 
 
-axios
-    .get(url)
-    .then(res => {
-        //console.log(res)
-        return res.data
-    })
-    .then(res => {
-        //console.log(res)
-        return res['list']
-    })
-    .then(res => {
-        console.log(res)
-    })
+// axios
+//     .get(url)
+//     .then(res => {
+//         //console.log(res)
+//         return res.data
+//     })
+//     .then(res => {
+//         //console.log(res)
+//         return res['list']
+//     })
+//     .then(res => {
+//         //console.log(res)
+//         return res
+//     })
+//     .then(res => {
+//         //exibir temperatura minima, maxima e descrição de cada previsao
+//         // for (let i = 0; i < res.length; i++){
+            
+//         // }
+//         for (previsao of res){
+//             console.log (`
+//                 Temp min: ${previsao.main.temp_min},
+//                 Temp max: ${previsao.main.temp_max},
+//                 Descrição: ${previsao.weather[0].description}
+//             `)
+//         }
+//     })
 
