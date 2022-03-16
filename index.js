4c8ed43d329d2ccdb4beeacc43399ac503711f93
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-16 16:29:51 -0300

    Estamos no bloco de codigo 3.3.5 da apostila 02, sobre JS. Ha um exercicio para ser feito, mostrando as previsoes

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 require('dotenv').config()
 const axios = require ('axios')
+// const pessoa = {
+//     nome: "Joao",
+//     idade: 17,
+//     altura: 180
+// }
+// const {idade, nome} = pessoa
 
+const { PROTOCOL, BASE_URL, APPID, UNITS, CNT, Q, LANGUAGE} = process.env
+
+const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&lang=${LANGUAGE}&units=${UNITS}`
+
+//https://exemplo.com.br/produtos
+// app.get ('/produtos', (req, res) => {
+//     // const resultado = axios.get(url)
+//     // console.log (resultado)
+//     // const soma = 2 + 3
+//     pegarResultadoDoBanco().then (resultado => res.send(200))
+// })
+
+// const futuro = new Date().getTime() + 20000;
+// while (new Date().getTime() <= futuro);
+// console.log(2 + 3)
+
+
+axios
+    .get(url)
+    .then(res => {
+        //console.log(res)
+        return res.data
+    })
+    .then(res => {
+        //console.log(res)
+        return res['list']
+    })
+    .then(res => {
+        console.log(res)
+    })
 
